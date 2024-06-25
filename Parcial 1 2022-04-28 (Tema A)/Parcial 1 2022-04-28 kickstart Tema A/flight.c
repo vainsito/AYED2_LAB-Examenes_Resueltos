dc9524b99e4886b1caef5057cf6f4c0d55a2f050
ezequiel <ezequiel.marin@mi.unc.edu.ar>
2024-06-25 18:16:52 -0300

    Agrego parcial 1 de 2022 tema A

---
+++ Parcial 1 2022-04-28 (Tema A)/Parcial 1 2022-04-28 kickstart Tema A/flight.c
@@ -0,0 +1,18 @@
+/*
+  @file layover.c
+  @brief Implements flight structure and methods
+*/
+#include <stdlib.h>
+#include "flight.h"
+
+static const int AMOUNT_OF_FLIGHT_VARS = 3 ;
+
+Flight flight_from_file(FILE* file, char code)
+{
+    Flight flight;
+    flight.code = code;
+
+    /* COMPLETAR */
+
+    return flight;
+}
